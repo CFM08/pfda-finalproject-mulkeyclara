f23f7128f2993ea71b72258d6fc8ff36556108a1
Clara Mulkey <clara.mulkey.05@gmail.com>
2025-05-11 08:55:52 -0500

    colors

--- project.py
+++ project.py
@@ -9,4 +9,14 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Color Memorization")
 TILE_SIZE = WIDTH // 3
 
-
+COLORS = [
+    (255, 0, 0), #red
+    (0, 255, 0), #green
+    (0, 0, 255), #blue
+    (0, 255, 255), #cyan
+    (255, 255, 0), #yellow
+    (0, 126, 126), #teal
+    (130, 0, 130), #purple
+    (255, 0, 265), #magenta
+    (255, 168, 0), #orange
+]
