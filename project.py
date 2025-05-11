7d0c07065f6728eba8fba196b13736413e2d51d9
Clara Mulkey <clara.mulkey.05@gmail.com>
2025-05-11 08:22:39 -0500

    establishing the frame

---
+++ project.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+import time
+
+pygame.init()
+
+WIDTH, HEIGHT = 600, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Color Memorization")
+TILE_SIZE = WIDTH // 3
+
+
