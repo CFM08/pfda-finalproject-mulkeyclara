23fe5315b216a8b2c1fdc8f8a155a36715511a16
Clara Mulkey <clara.mulkey.05@gmail.com>
2025-05-12 17:43:42 -0500

    messing with heights

--- src/project.py
+++ src/project.py
@@ -4,7 +4,7 @@ import time
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 1200, 800
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Color Memorization")
 TILE_SIZE = WIDTH // 3
